cff83a0c64d2316136fb705bcedf3322002f478d
James <james@Zipcoders-MacBook-Pro-101.local>
2023-03-14 20:46:50 -0400

    added scanner for number tosses

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -5,34 +5,35 @@ public class Dice {
 
     public int rollTotal = 0;
     public int newRoll = 0;
-    public static int numberOfDice = 0;
+    public static int numberDice = 0;
     public int lowest;
     public int highest;
     Bins bin;
-    Scanner scan = new Scanner(System.in);
+
     public Dice(){
     }
 
     public void howManyDice(Scanner scan){
         // Ask for number of dice to toss;
-        System.out.println("How many dice are we tossing today?");
-        int scan = scan.nextInt();
+
     }
     public void setNumberDice(int x){
-        numberOfDice = x;
+        numberDice = x;
     }
     public int getNumberOfDice(){
-        return numberOfDice;
+        return numberDice;
     }
-    public int rollDie() {
+    public int rollDie(int dice) {
         Random roll = new Random();
-        for (int i = 1; i <= numberOfDice; i++) {
+        for (int i = 1; i <= dice; i++) {
             newRoll = roll.nextInt(6) + 1;
                 rollTotal+= newRoll;
         }
         return rollTotal;
     }
-    public
+    public Integer tossAndSum(){
+
+    }
 
 
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,15 +1,40 @@
 import java.util.Map;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Simulation {
+    private static int numberDice;
+    private static int numberTosses;
 
-    public Simulation()
+
+    public Simulation(){
+        this.numberDice = 2;
+        this.numberTosses = 1000000;
+    }
+
+    public Simulation(int numberDice, int numberTosses) {
+        this.numberDice = numberDice;
+        this.numberTosses = numberTosses;
+    }
 
     public static void main(String[] args) {
-
-        int input = scan.nextInt();
+        Scanner scan = new Scanner(System.in);
         Dice dice = new Dice();
         Bins bins = new Bins(dice.getNumberOfDice());
+        System.out.println("How many dice are we tossing today?");
+        String input = scan.nextLine();
+        System.out.println("How many tosses?");
+        String input2 = scan.nextLine();
+        if (input.isEmpty() || input2.isEmpty()) {
+            numberTosses = 1000000;
+            numberDice = 2;
+            } else {
+            numberDice = Integer.parseInt(input);
+            numberTosses = Integer.parseInt(input2);
+        }
+
+
+
 
         // get number of die to roll
         dice.setNumberDice(input);
